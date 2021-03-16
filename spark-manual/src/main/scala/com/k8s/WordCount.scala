96dfdf3ab1634ab9009af1ebf0520d9409f05199
yang040840219 <yxl040840219@126.com>
2021-03-16 14:36:13 +0800

    manual

---
+++ spark-manual/src/main/scala/com/k8s/WordCount.scala
@@ -0,0 +1,23 @@
+// scalastyle:off
+package com.k8s
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.count
+
+object WordCount {
+  
+  def main(args: Array[String]): Unit = {
+	val spark = SparkSession.builder().getOrCreate()
+	import spark.implicits._
+	val df = (0 until 100000).toDF("id").selectExpr("id % 5 as key", "id%10 as value")
+	  .groupBy("key").agg(count("value").as("cnt"))
+	  .repartition(1).mapPartitions(iter => {
+	  iter.map(row => {
+		(row.getAs[Int]("key"), row.getAs[Long]("cnt"))
+	  })
+	}).toDF("key", "value")
+	df.show()
+	spark.stop()
+  }
+  
+}
